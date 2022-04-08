24381bffe6e94594650d5eebddcb3a4e96a6b769
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2022-04-08 17:03:11 +0100

    Fullscreen & Resolution Working (Kinda)
    Fullscreen toggles as does resolution, however, it changes the position of the resources on screen

--- engine/system_renderer.cpp
+++ engine/system_renderer.cpp
@@ -1,4 +1,5 @@
 #include "system_renderer.h"
+#include "engine.h"
 #include <queue>
 
 using namespace std;
@@ -20,6 +21,25 @@ void Renderer::render() {
   if (rw == nullptr) {
     throw("No render window set! ");
   }
+  //set view size in case window size has been changed
+  if (Engine::user_preferences.changed_resolution || Engine::user_preferences.changed_fullscreen) {
+	  if (Engine::user_preferences.fullscreen == 8) {
+		  Engine::GetWindow().create(VideoMode(Engine::user_preferences.video_resolution.x, Engine::user_preferences.video_resolution.y), "Baguette Her Back!", Engine::user_preferences.fullscreen);
+		  Engine::user_preferences.changed_fullscreen = false;
+		  Engine::user_preferences.changed_resolution = false;
+	  }
+	  else if (Engine::user_preferences.fullscreen == 7) {
+		  if (Engine::user_preferences.changed_resolution) {
+			  Engine::user_preferences.changed_resolution = false;
+		  }
+		  else {
+			  Engine::GetWindow().create(VideoMode(Engine::user_preferences.video_resolution_default.x, Engine::user_preferences.video_resolution_default.y), "Baguette Her Back!", Engine::user_preferences.fullscreen);
+			  Engine::user_preferences.changed_fullscreen = false;
+		  }
+
+	  }
+  }
+
   while (!sprites.empty()) {
     rw->draw(*sprites.front());
     sprites.pop();
--- engine/user_preferences.cpp
+++ engine/user_preferences.cpp
@@ -6,5 +6,5 @@ using namespace std;
 
 UserPreferences::UserPreferences() :
 	// Fullscreen default = 7 (OFF), Fullscreen is activated when fullscreen(8)
-	fullscreen(7), video_resolution(Vector2f(800,600)), changed_fullscreen(false), changed_resolution(false){
+	fullscreen(7), video_resolution(Vector2f(2,2)), changed_fullscreen(false), changed_resolution(false){
 } 
