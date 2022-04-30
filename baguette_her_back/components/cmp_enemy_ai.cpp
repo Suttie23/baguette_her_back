f00c9c00e8281fa9891ebc7269b8b90e97ba1c81
Mikolaj-Figiel <78086404+Mikolaj-Figiel@users.noreply.github.com>
2022-04-30 19:07:59 +0100

    proper ai backup

--- baguette_her_back/components/cmp_enemy_ai.cpp
+++ baguette_her_back/components/cmp_enemy_ai.cpp
@@ -1,54 +1,63 @@
-#include "cmp_enemy_ai.h"
 
-using namespace std;
-using namespace sf;
+#include "cmp_enemy_ai.h"
+#include <unordered_set>
+
+
+size_t operator==(Location const& a, Location const& b) {
+    return (a.x == b.x && a.y == b.y);
+}
+
+size_t operator<(Location const& a, Location const& b) {
+    return (a.y < b.y);
+}
+
+array<Location, 4> DIRS = {
+    /* East, West, North, South */
+    Location{30, 0}, Location{-30, 0},
+    Location{0, -30}, Location{0, 30}
+};
 
 void EnemyAIComponent::update(double dt) {
-<<<<<<< Updated upstream
-=======
-    //if (route.empty()) {
-       // route = pathFinding();
-    //}
-   // Vector2f disp;
-    //disp.x = (route.front().x * 1.f);
-    //disp.y = (route.front().y * 1.f);
-    //move((_parent->getPosition()+(disp - _parent->getPosition())));
-    //route.front() = std::move(route.back());
-    //route.pop_back();
-   
-    //gets current direction and multiplies by speed and dt
-    //adds a buffer of 16f before checking if it'll end up in a wall
->>>>>>> Stashed changes
-  auto mov = _direction * (float)(dt * _speed);
-  mov.x += _direction.x * 16.f;
-  if (!validMove(_parent->getPosition() + mov)) {
-    _direction *= -1.f;
-  }
+    if (route.empty()) {
+        route = pathFinding();
+    }
+    // Vector2f disp;
+     //disp.x = (route.front().x * 1.f);
+     //disp.y = (route.front().y * 1.f);
+     //move((_parent->getPosition()+(disp - _parent->getPosition())));
+     //route.front() = std::move(route.back());
+     //route.pop_back();
 
-  move(_direction * (float)(dt * _speed));
-  ActorMovementComponent::update(dt);
+     //gets current direction and multiplies by speed and dt
+     //adds a buffer of 16f before checking if it'll end up in a wall
+    auto mov = _direction * (float)(dt * _speed);
+    mov.x += _direction.x * 16.f;
+    if (!validMove(_parent->getPosition() + mov)) {
+        _direction *= -1.f;
+    }
+
+    //move(_direction * (float)(dt * _speed));
+    ActorMovementComponent::update(dt);
 }
 
 EnemyAIComponent::EnemyAIComponent(Entity* p) : ActorMovementComponent(p) {
-  _direction = Vector2f(1.0f, 0);
-  _speed = 100.0f;
+    _direction = Vector2f(1.0f, 0);
+    _speed = 100.0f;
 }
-<<<<<<< Updated upstream
-=======
 
 std::vector<Location> EnemyAIComponent::pathFinding() {
     Vector2f begin = (_parent->getPosition());
     Location start;
 
-    start.x = (int) floor(begin.x);
-    start.y = (int) floor(begin.y);
+    start.x = (int)floor(begin.x);
+    start.y = (int)floor(begin.y);
 
     Location goal;
-    shared_ptr<Entity> player = _player.lock();
+    auto player = _player.lock();
     Vector2f end = (player->getPosition());
-    goal.x = (int) floor(end.x);
-    goal.y = (int) floor(end.y);
-   
+    goal.x = (int)floor(end.x);
+    goal.y = (int)floor(end.y);
+
     unordered_map<Location, Location> came_from;
     unordered_map<Location, int> cost_so_far;
     PriorityQueue<Location, double> frontier;
@@ -60,7 +69,7 @@ std::vector<Location> EnemyAIComponent::pathFinding() {
     while (!frontier.empty()) {
         Location current = frontier.get();
 
-        if ((current.x == goal.x && current.y == goal.y)||(dist(current, goal) < 20)) {
+        if ((current.x == goal.x && current.y == goal.y) || (dist(current, goal) < 20)) {
             break;
         }
 
@@ -120,10 +129,3 @@ int EnemyAIComponent::cost(Location from_node, Location to_node) {
 inline double EnemyAIComponent::heuristic(Location a, Location b) {
     return abs(a.x - b.x) + abs(a.y - b.y);
 }
-
-
-
-
-
-
->>>>>>> Stashed changes
--- baguette_her_back/components/cmp_enemy_ai.h
+++ baguette_her_back/components/cmp_enemy_ai.h
@@ -1,15 +1,77 @@
 #pragma once
 
+
 #include "cmp_actor_movement.h"
+#include <ecm.h>
+#include <array>
+#include <unordered_map>
+#include <queue>
+#include <vector>
+
+
+
+using namespace std;
+using namespace sf;
+
+
+struct Location {
+
+    int x, y;
+};
+
+template <> struct hash<Location> {
+    size_t operator () (const Location& id) const noexcept {
+
+        return hash<int>() (id.x ^ (id.y << 16));
+    }
+};
 
 class EnemyAIComponent : public ActorMovementComponent {
 protected:
-  sf::Vector2f _direction;
-
+    sf::Vector2f _direction;
+    std::weak_ptr<Entity> _player;
+    std::vector<Location> route;
 public:
-  void update(double dt) override;
+    void update(double dt) override;
 
-  explicit EnemyAIComponent(Entity* p);
+    explicit EnemyAIComponent(Entity* p);
 
-  EnemyAIComponent() = delete;
+    EnemyAIComponent() = delete;
+
+    std::vector<Location> neighbors(Location id);
+
+    int cost(Location from_node, Location to_node);
+
+    inline double heuristic(Location a, Location b);
+
+    std::vector<Location> pathFinding();
+
+    double dist(Location a, Location b) {
+        int i = a.x;
+        int j = a.y;
+        int k = b.x;
+        int l = b.y;
+
+        return sqrt((k - i) ^ 2 + (l - j) ^ 2);
+    };
+};
+
+template<typename T, typename priority_t>
+struct PriorityQueue {
+    typedef pair<priority_t, T> PQElement;
+    priority_queue<PQElement, vector<PQElement>, greater<PQElement>> elements;
+
+    inline bool empty() const {
+        return elements.empty();
+    }
+
+    inline void put(T item, priority_t priority) {
+        elements.emplace(priority, item);
+    }
+
+    T get() {
+        T best_item = elements.top().second;
+        elements.pop();
+        return best_item;
+    }
 };
